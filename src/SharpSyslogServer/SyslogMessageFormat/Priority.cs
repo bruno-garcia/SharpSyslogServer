ee354184ded0af7f7abfff32380148ea046ce700
Bruno Garcia <bruno@brunogarcia.com>
2016-04-03 01:34:09 +0200

    Created syslog message format as per RFC 5424

---
+++ src/SharpSyslogServer/SyslogMessageFormat/Priority.cs
@@ -0,0 +1,17 @@
+﻿namespace SharpSyslogServer.SyslogMessageFormat
+{
+    /// <summary>
+    /// Message Priority, Composed by Facility and Severity
+    /// </summary>
+    /// <remarks>
+    /// Facility and Severity values are not normative but often used.  They
+    /// are described in the following tables for purely informational
+    /// purposes. 
+    /// https://tools.ietf.org/html/rfc5424#section-6.2.1
+    /// </remarks>
+    public class Priority
+    {
+        public Facility Facility { get; set; }
+        public Severity Severity { get; set; }
+    }
+}
---
+++ src/SharpSyslogServer/SyslogMessageFormat/SyslogMessage.cs
@@ -0,0 +1,18 @@
+﻿namespace SharpSyslogServer.SyslogMessageFormat
+{
+    /// <summary>
+    /// A Syslog Message as per RFC 5424
+    /// </summary>
+    public class SyslogMessage
+    {
+        public Header Header { get; set; }
+        /// <summary>
+        /// Structured Data/Element
+        /// </summary>
+        public StructuredData StructuredData { get; set; }
+        /// <summary>
+        /// The syslog Message text
+        /// </summary>
+        public string Message { get; set; }
+    }
+}
